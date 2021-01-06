51bd3bffa68d569af52b3b597578248426af0624
Tecelli Akıntuğ <tecelli.akintug@gmail.com>
2021-01-06 23:04:23 +0300

    Fixed indentation level

--- server.py
+++ server.py
@@ -12,47 +12,47 @@ from movie import Movie
 import models
 
 def create_app():
-  # Configure flask app
-  app = Flask(__name__)
-  app.add_url_rule("/", view_func = view.home_page)
-  app.add_url_rule("/movies", view_func = view.movies_page, methods=["GET", "POST"])
-  app.add_url_rule("/movies/<int:movie_key>", view_func = view.movie_page)
-  app.add_url_rule("/new-movie", view_func=view.movie_add_page, methods=["GET", "POST"])
-  app.add_url_rule("/image/<filename>", view_func=view.image_server)
-  app.add_url_rule("/signup", view_func=view.signup_page, methods=["GET", "POST"])
+    # Configure flask app
+    app = Flask(__name__)
+    app.add_url_rule("/", view_func = view.home_page)
+    app.add_url_rule("/movies", view_func = view.movies_page, methods=["GET", "POST"])
+    app.add_url_rule("/movies/<int:movie_key>", view_func = view.movie_page)
+    app.add_url_rule("/new-movie", view_func=view.movie_add_page, methods=["GET", "POST"])
+    app.add_url_rule("/image/<filename>", view_func=view.image_server)
+    app.add_url_rule("/signup", view_func=view.signup_page, methods=["GET", "POST"])
 
-  # Configure temporary database
-  db = Database()
-  db.add_movie(Movie("Slaughterhouse-Five", year = 1972))
-  db.add_movie(Movie("The Shining"))
-  app.config["db"] = db
+    # Configure temporary database
+    db = Database()
+    db.add_movie(Movie("Slaughterhouse-Five", year = 1972))
+    db.add_movie(Movie("The Shining"))
+    app.config["db"] = db
 
-  # Configure flask login
-  # login = LoginManager(app)
-  # login.init_app(app)
+    # Configure flask login
+    # login = LoginManager(app)
+    # login.init_app(app)
 
-  # Get Settings
-  app.config.from_object("settings")
-  return app
+    # Get Settings
+    app.config.from_object("settings")
+    return app
 
 
 if __name__ == "__main__":
 
 
-  db = Database()
-  active_jams = db.GetUpcomingGameJams()
-  if(len(active_jams) == 0):
-    print('No upcoming jams')
-  else:
-    for jam in active_jams:
-      print('{}: {} - {} -> {} = {}'.format(jam.id, jam.name, jam.startDate, jam.endDate, jam.endDate - jam.startDate))
+    db = Database()
+    active_jams = db.GetUpcomingGameJams()
+    if(len(active_jams) == 0):
+        print('No upcoming jams')
+    else:
+        for jam in active_jams:
+            print('{}: {} - {} -> {} = {}'.format(jam.id, jam.name, jam.startDate, jam.endDate, jam.endDate - jam.startDate))
 
 
 
-  del db
+    del db
 
 
-  # print(mycursor.rowcount, "users found.")
-  # app = create_app()
-  # app.run(host = "0.0.0.0", port = 8080)
+    # print(mycursor.rowcount, "users found.")
+    # app = create_app()
+    # app.run(host = "0.0.0.0", port = 8080)
 
